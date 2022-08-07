806a2f341198795a84c9d666ac305b6d7be3ac31
StefanTrpkovic <stefan.trpkovik@gmail.com>
2022-08-07 21:02:42 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import AdComponent from "./AdComponent";
+import { possibleAnswers } from "./constants";
 
-function App() {
+export default function App() {
+  const [userInput, setUserInput] = useState("");
+  const [randomIndex, setRandomIndex] = useState(0);
+
+  const ask = () => {
+    if (userInput) {
+      let rndNumber = Math.floor(Math.random() * 20);
+      while (rndNumber === 0) rndNumber = Math.floor(Math.random() * 20);
+      setRandomIndex(rndNumber);
+      setUserInput("");
+    }
+  };
+
+  const handleKeypress = (e) => {
+    if (e.keyCode === 13) ask();
+  };
+
+  const handleChange = (event) => setUserInput(event.target.value);
+
+  const answer = possibleAnswers[randomIndex];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="question-container1">
+      <div className="question-headers">
+        <h2>Сконцентрирај се убаво.</h2>
+        <h2>
+          Помисли на прашање, постави го и магичната топка ќе ти го покаже
+          патот.
+        </h2>
+      </div>
+      <div className="question-container">
+        <input
+          className="input"
+          type="text"
+          value={userInput}
+          onChange={handleChange}
+          onKeyDown={handleKeypress}
+          placeholder="Постави прашање"
+        />
+        <button className="input" onClick={ask}>
+          Кажи ми кажи!
+        </button>
+      </div>
+
+      <div className="ball-container">
+        <div className="ball-black-outer">
+          <div className="ball-white-inner">
+            <div>
+              {answer !== "8" ? answer : <div className="eight">8</div>}
+            </div>
+          </div>
+        </div>
+        <div className="ball-shadow"></div>
+      </div>
+      <AdComponent />
     </div>
   );
 }
-
-export default App;
